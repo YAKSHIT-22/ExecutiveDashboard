90e1a16179385064b96d8c7f2d290a9f4ed97827
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-07-03 20:55:36 +0530

    backend attached

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Route, Routes } from 'react-router-dom'
+import PrivateRoute from "./components/PrivateRoute";
+import RouteAccess from "./hooks/RouteAccess";
+import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
+import Error from "./Pages/Error";
+import SignUp from "./Pages/Signup";
+import ForgetPassword from "./Pages/ForgetPassword";
+import PublicRoute from "./hooks/PublicRoute";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      
+        <Route path="/" element={<PublicRoute><Login /></PublicRoute>} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgetpassword" element={<ForgetPassword />} />
+      
+      <Route path="/profile" element={<PrivateRoute />} >
+     <Route path="/profile" element={<Profile />} />
+    </Route>
+    <Route path="*" element={<Error/>} />
+    </Routes>
+    
   );
 }
 
